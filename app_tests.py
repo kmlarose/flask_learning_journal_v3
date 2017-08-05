f959d48548385348dc82aa034bd5e670e0558c5b
Ken LaRose <kmlarose2@gmail.com>
2017-08-05 11:05:10 -0500

    layout template for head, header, and footer

---
+++ app_tests.py
@@ -0,0 +1,26 @@
+# Project Requirements
+#######################
+# Test Peewee model class for journal entries
+
+# Create “list” view using the route /entries.
+# The list view contains a list of journal entries, which displays Title and Date for Entry.
+# Title should be hyperlinked to the detail page for each journal entry.
+# Include a link to add an entry.
+
+# Create “details” view with the route “/details” displaying the journal entry with all fields:
+# Title, Date, Time Spent, What You Learned, Resources to Remember.
+# Include a link to edit the entry.
+
+# Create “add/edit” view with the route “/entry”
+# that allows the user to add or edit journal entry with the following fields:
+# Title, Date, Time Spent, What You Learned, Resources to Remember.
+# Add the ability to delete a journal entry.
+
+# Use the supplied HTML/CSS to build and style your pages.
+# Use CSS to style headings, font colors, journal entry container colors, body colors.
+
+# Add tags to journal entries in the model.
+# Add tags to journal entries on the listing page and allow the tags to be links to a list of specific tags.
+# Add tags to the details page.
+# Create password protection or user login (provide credentials for code review).
+# Routing uses slugs.
---
+++ learning_journal.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+
+# set up the flask app
+DEBUG = True
+PORT = 8000
+HOST = '0.0.0.0'
+
+app = Flask(__name__)
+app.secret_key = '6:YSE)+*gEeY5@-H/e*7v6$JZe\pbE-['
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=DEBUG, host=HOST, port=PORT)
